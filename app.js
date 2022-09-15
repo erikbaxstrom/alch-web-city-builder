0cc0f254f3a0b8b01e9bb195f8e56b9a9dcdae98
Erik M Baxstrom <baxstrem@gmail.com>
2022-09-15 14:39:50 -0700

    display city from state object

--- app.js
+++ app.js
@@ -1,11 +1,28 @@
 /* Imports */
 
 /* Get DOM Elements */
+const cityName = document.getElementById('city-name');
+const climatePhoto = document.getElementById('climate-photo');
+const archPhoto = document.getElementById('arch-photo');
+const attractionsList = document.getElementById('attractions-list');
 
 /* State */
+let city = {
+    name: 'Olympia',
+    climate: 'tropical',
+    architecture: 'surreal',
+    attractions: ['Capital Building', 'Capital Lake', 'Olympia Brewery'],
+};
 
 /* Events */
 
 /* Display Functions */
+function displayCity() {
+    cityName.textContent = city.name;
+    climatePhoto.src = './assets/climate/climate-' + city.climate + '.jpg';
+    climatePhoto.alt = city.climate;
+    archPhoto.src = './assets/architecture/arch-' + city.architecture + '.jpg';
+}
 
 // (don't forget to call any display functions you want to run on page load!)
+displayCity();
